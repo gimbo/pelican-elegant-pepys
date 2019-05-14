0d1ac22177b9903da5a6c12e0744e4d9ba48777e
Andy Gimblett <andy@barefootcode.com>
2019-05-15 00:04:30 +0100

    Add Dockerfile and pelicanconf.py for pepys builds

---
+++ Dockerfile
@@ -0,0 +1,37 @@
+FROM python:3.7.3-alpine3.9
+
+# Install pelican plugins via subversion
+RUN \
+        apk update && \
+        apk add subversion && \
+        # -r14 is ad7c9879147bf82dc8c66e50ef464ecec4c8972c
+        svn export -r14 https://github.com/akhayyat/pelican-page-hierarchy.git/trunk /usr/src/pelican-plugins/pelican-page-hierarchy && \
+        # -r1436 is dc39e9af88d3c79c43495fd872ae0ad8d985337c
+        svn export -r1436 https://github.com/getpelican/pelican-plugins.git/trunk/neighbors /usr/src/pelican-plugins/neighbors && \
+        svn export -r1436 https://github.com/getpelican/pelican-plugins.git/trunk/tipue_search /usr/src/pelican-plugins/tipue_search && \
+        apk del subversion && \
+        rm -rf /var/cache/apk/*
+
+# Install pelican and related packages
+RUN \
+        pip install --no-cache-dir \
+        pelican==4.0.1 \
+        beautifulsoup4==4.7.1 \
+        Markdown==3.1 \
+        mdx_truly_sane_lists==1.2 \
+        pymdown-extensions==6.0 \
+        typogrify==2.0.7
+
+# Install theme
+COPY . /usr/src/theme
+
+# Set up default env vars
+ENV \
+        PELICAN_CONTENT=/usr/content \
+        PELICAN_OUTPUT_PATH=/usr/build \
+        PELICAN_PLUGIN_PATHS=/usr/src/pelican-plugins \
+        PELICAN_THEME=/usr/src/theme \
+        PELICAN_PEPYS_CACHES_PATH=/usr/caches
+
+# By default, just run pelican vs the built-in configuration
+CMD pelican -s /usr/src/theme/pelicanconf.py
---
+++ pelicanconf.py
@@ -0,0 +1,78 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+
+import os
+import tempfile
+
+
+SITENAME = os.environ['PELICAN_SITENAME']
+AUTHOR = os.environ['PELICAN_AUTHOR']
+SITEURL = os.environ['PELICAN_SITEURL']
+PATH = os.path.expanduser(os.environ['PELICAN_CONTENT'])
+ARTICLE_EXCLUDES = PAGE_EXCLUDES = os.environ.get('PELICAN_EXCLUDES', '').split(':')
+OUTPUT_PATH = os.path.expanduser(os.environ['PELICAN_OUTPUT_PATH'])
+PLUGIN_PATHS = [
+    os.path.expanduser(plugin_path)
+    for plugin_path in os.environ['PELICAN_PLUGIN_PATHS'].split(':')
+]
+
+THEME = os.path.expanduser(os.environ.get('PELICAN_THEME', 'notmyidea'))
+
+
+DEFAULT_LANG = 'en'
+TIMEZONE = 'Europe/London'
+
+DEFAULT_PAGINATION = 30
+
+ARTICLE_URL = '{date:%Y}/{date:%m}/{date:%d}/{slug}/'
+ARTICLE_SAVE_AS = '{date:%Y}/{date:%m}/{date:%d}/{slug}/index.html'
+YEAR_ARCHIVE_SAVE_AS = '{date:%Y}/index.html'
+MONTH_ARCHIVE_SAVE_AS = '{date:%Y}/{date:%m}/index.html'
+DAY_ARCHIVE_SAVE_AS = '{date:%Y}/{date:%m}/{date:%d}/index.html'
+CATEGORY_URL = '{slug}/'
+AUTHOR_URL = ''
+AUTHOR_SAVE_AS = ''
+
+DEFAULT_DATE = 'fs'
+FILENAME_METADATA = r'(?P<title>(?P<date>\d{4}\.\d{2}\.\d{2})?.*)'
+
+try:
+    cache_tag = os.environ['PELICAN_PEPYS_CACHE_TAG']
+    caches_path = os.environ.get(
+        'PELICAN_PEPYS_CACHES_PATH',
+        os.path.join(tempfile.gettempdir(), 'pelipepys_caches')
+    )
+    CACHE_PATH = os.path.join(caches_path, cache_tag)
+    CACHE_CONTENT = True
+    LOAD_CONTENT_CACHE = True
+    CHECK_MODIFIED_METHOD = 'mtime'
+    # CONTENT_CACHING_LAYER = 'generator'
+    # WITH_FUTURE_DATES = False
+except KeyError:
+    # Caching disabled
+    DELETE_OUTPUT_DIRECTORY = True
+
+# No feeds for diaries
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+PLUGINS = ['neighbors', 'tipue_search']
+DIRECT_TEMPLATES = ['index', 'tags', 'categories', 'authors', 'archives', 'search']
+
+MARKDOWN = {
+    'extension_configs': {
+        'markdown.extensions.codehilite': {
+            'css_class': 'highlight',
+        },
+        'markdown.extensions.extra': {},
+        'markdown.extensions.meta': {},
+        'markdown.extensions.smarty': {},
+        'mdx_truly_sane_lists': {},
+        'pymdownx.magiclink': {},
+    },
+    'output_format': 'html5',
+}
